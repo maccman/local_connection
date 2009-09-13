7bd19076f5ffe28a470e698c5eb5fda260528e36
Alex MacCaw <maccman@gmail.com>
2009-09-13 10:41:09 +0100

    Implement Darwin first

--- local_connection.rb
+++ local_connection.rb
@@ -2,17 +2,34 @@ require 'thread'
 # require 'rubyamf'
 
 class LocalConnection
+  attr_reader :running, :name
+  
   def initialize(name, &block)
-    @name = name
+    @name     = name
     @callback = block
+    @running  = false
+    @messages = []
+  end
+  
+  def write(ob)
+    raise "Connect first" unless running
+    @messages << ob
   end
   
   def connect
-    
+    @running = true
+    register
+    Thread.new do
+      while @running
+        poll
+        sleep 0.1
+      end
+    end
   end
   
   def disconnect
-    
+    @running = false
+    deregister
   end
   
   private
@@ -29,10 +46,34 @@ class LocalConnection
     end
   
     def poll
+      # Lock
+      # Check shared memory
+      # Execute callbacks if result
+      sync {
+        
+      }
+    end
+    
+    def shared_memory
+      @shared_memory ||= begin
+        sm = SysVIPC::SharedMemory.new( 0x53414E44, 0 )
+        sm.attach
+        sm
+      end
+    end
+    
+    def read_memory
       
     end
     
     def sync(&block)
-      
+      @sem ||= begin
+        SysVIPC::Semaphore.new(
+          "MacromediaSemaphoreDig", 
+          1, 
+          SysVIPC::IPC_CREAT | 0600
+        )
+      end
+      @sem.lock { yield }
     end
 end
